6729e206856f349c948c6496683a1047ce1eb1f1
Marco Bonomo <marco.bonomo@gmail.com>
2021-12-17 09:47:52 +0100

    ita eng - hover e active

--- components/MenuPage.vue
+++ components/MenuPage.vue
@@ -5,9 +5,9 @@
         >
             <AurelioLogo></AurelioLogo>
         </header>
-        <div class="text-center py-4 text-pale-leaf-800 text-2xl">
-            <nuxt-link to="/">IT</nuxt-link>&nbsp;|&nbsp;
-            <nuxt-link to="/en">EN</nuxt-link>
+        <div class="text-center py-4 text-pale-leaf-500 text-2xl hover:text-pale-leaf-600">
+            <nuxt-link to="/">ITA</nuxt-link>&nbsp;|&nbsp;
+            <nuxt-link to="/en">ENG</nuxt-link>
         </div>
         <div class="my-8 grid gap-4 grid-cols-1 max-w-lg mx-auto">
             <slot></slot>
@@ -21,6 +21,6 @@ export default {
 </script>
 <style>
 .router-link-active {
-    @apply text-pale-leaf-600;
+    @apply text-pale-leaf-800 underline;
 }
 </style>
