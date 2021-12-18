5dce4cc479b36a6685850ffddc6f41eaf1e43ac6
Marco Bonomo <marco.bonomo@gmail.com>
2021-12-18 11:46:28 +0100

    updated wine list

--- pages/en.vue
+++ pages/en.vue
@@ -1,7 +1,7 @@
 <template>
     <MenuPage>
         <MenuLink link="/MENU-Restaurant-Da-Aurelio.pdf">menu</MenuLink>
-        <MenuLink link="/LISTA-VINI-Ristorante-Da-Aurelio.pdf">Wine list</MenuLink>
+        <MenuLink link="/WINE-LIST-Da-Aurelio.pdf">Wine list</MenuLink>
     </MenuPage>
 </template>
 <script>
