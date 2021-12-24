7fe4a1981179728a3550753ecea269ae291a476a
Marco Bonomo <marco.bonomo@gmail.com>
2021-12-24 11:18:36 +0100

    wine list 2021-2022

--- pages/en.vue
+++ pages/en.vue
@@ -1,7 +1,7 @@
 <template>
     <MenuPage>
         <MenuLink link="/MENU-Ristorante-Da-Aurelio-Natale-2021.pdf">menu</MenuLink>
-        <MenuLink link="/Lista_Vini_Da-Aurelio_Inverno_2021-2022.pdf">Wine list</MenuLink>
+        <MenuLink link="/Wine_List_Da-Aurelio_Winter-2021-2022.pdf">Wine list</MenuLink>
     </MenuPage>
 </template>
 <script>
