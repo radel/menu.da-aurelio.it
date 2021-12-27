fac8c95ef9009fdb5dc7f0bbb9336130ef31b7a0
Marco Bonomo <marco.bonomo@gmail.com>
2021-12-27 16:03:48 +0100

    menu inglese

--- pages/en.vue
+++ pages/en.vue
@@ -1,6 +1,6 @@
 <template>
     <MenuPage>
-        <MenuLink link="/MENU-Ristorante-Da-Aurelio-Natale-2021.pdf">menu</MenuLink>
+        <MenuLink link="/MENU-Restaurant-Da-Aurelio.pdf">menu</MenuLink>
         <MenuLink link="/Wine_List_Da-Aurelio_Winter-2021-2022.pdf">Wine list</MenuLink>
     </MenuPage>
 </template>
