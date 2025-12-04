b70863bbcb167aa9c43d7efe6c10aec3aa5821b0
Marco Bonomo <marco.bonomo@gmail.com>
2025-12-04 18:33:59 +0100

    removed wine list

--- pages/en.vue
+++ pages/en.vue
@@ -1,26 +1,23 @@
 <template>
     <MenuPage>
         <MenuLink :link="`/en-menu.pdf?${rand(1000)}`">menu</MenuLink>
-        <MenuLink :link="`/en-wine.pdf?${rand(1000)}`">Wine list</MenuLink>
         <Social>Follow us</Social>
     </MenuPage>
 </template>
 <script setup>
 const rand = function (max) {
-  return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 }
 useHead({
-  title: 'Menu- Restaurant Da Aurelio - Giau Pass',
-  // or, instead:
-  // titleTemplate: (title) => `My App - ${title}`,
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
-  charset: 'utf-8',
-  meta: [
-    { name: 'description', content: 'Menu - Restaurant Da Aurelio - Giau Pass' }
-  ],
+    title: 'Menu- Restaurant Da Aurelio - Giau Pass',
+    // or, instead:
+    // titleTemplate: (title) => `My App - ${title}`,
+    viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    charset: 'utf-8',
+    meta: [
+        { name: 'description', content: 'Menu - Restaurant Da Aurelio - Giau Pass' }
+    ],
 })
 </script>
 
-<style lang="css">
-</style>
-
+<style lang="css"></style>
--- pages/it.vue
+++ pages/it.vue
@@ -1,27 +1,24 @@
 <template>
     <MenuPage>
         <MenuLink :link="`/it-menu.pdf?${rand(1000)}`">menu</MenuLink>
-        <MenuLink :link="`/it-vini.pdf?${rand(1000)}`">lista vini</MenuLink>
         <Social>Seguici su</Social>
     </MenuPage>
 </template>
 <script setup>
 const rand = function (max) {
-  return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 }
 useHead({
-  title: 'Menu - Ristorante Da Aurelio - Passo Giau',
-  // or, instead:
-  // titleTemplate: (title) => `My App - ${title}`,
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
-  charset: 'utf-8',
-  meta: [
-    { name: 'description', content: 'Menu - Ristorante Da Aurelio - Passo Giau' }
-  ],
+    title: 'Menu - Ristorante Da Aurelio - Passo Giau',
+    // or, instead:
+    // titleTemplate: (title) => `My App - ${title}`,
+    viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    charset: 'utf-8',
+    meta: [
+        { name: 'description', content: 'Menu - Ristorante Da Aurelio - Passo Giau' }
+    ],
 })
 </script>
 
 
-<style lang="css">
-</style>
-
+<style lang="css"></style>
